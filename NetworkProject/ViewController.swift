dd41cd402c9372f06ea1fa2538cd49187d5435cd
Muslim Gapparov <muslimgapparov@gmail.com>
2024-03-19 01:51:06 +0300

    Initial Commit

---
+++ NetworkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkProject
+//
+//  Created by Муслим Гаппаров on 3/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
